4ce90bf0814f1fe3e87715d30350f82dfed78e5f
Aga <agns22@wp.pl>
2017-06-13 00:01:14 +0200

    gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = function(grunt) {
+
+  // Project configuration.
+  
+  grunt.initConfig({
+  	sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'style.css': 'style.sass'
+        }
+      }
+    },
+    jshint: {
+      all: ['js/*.js']
+    },
+    watch: {
+          scripts: {
+              files: ['*.sass'],
+              tasks: ['sass'],
+              options: {
+                  spawn: false,
+            },
+        } 
+    }
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  
+  
+
+  
+  // Default task(s).
+  grunt.registerTask('default', ["sass","jshint", "watch"]);
+};
